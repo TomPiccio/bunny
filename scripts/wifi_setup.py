59658ed7e56fb50fdcc4a59674bfc59527455eee
Tom Piccio <75068584+TomPiccio@users.noreply.github.com>
2025-09-25 02:06:56 +0800

    feat: init wifi_setup.py

---
+++ scripts/wifi_setup.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, render_template_string
+import os
+
+app = Flask(__name__)
+
+HTML_FORM = """
+<!DOCTYPE html>
+<html>
+<body>
+  <h2>Bunny Wi-Fi Setup</h2>
+  <form action="/save" method="post">
+    SSID: <input type="text" name="ssid"><br><br>
+    Password: <input type="password" name="password"><br><br>
+    <input type="submit" value="Save">
+  </form>
+</body>
+</html>
+"""
+
+@app.route("/")
+def index():
+    return HTML_FORM
+
+@app.route("/save", methods=["POST"])
+def save():
+    ssid = request.form["ssid"]
+    password = request.form["password"]
+
+    wpa_conf = f"""
+ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=1
+country=US
+
+network={{
+    ssid="{ssid}"
+    psk="{password}"
+}}
+"""
+    with open("/etc/wpa_supplicant/wpa_supplicant.conf", "w") as f:
+        f.write(wpa_conf)
+
+    os.system("sync")
+    os.system("reboot")
+    return "Saved! Rebooting..."
