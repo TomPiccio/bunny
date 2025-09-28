00dee4336724a3196adbbddd2b85badfe58c8cb8
Tom Piccio <75068584+TomPiccio@users.noreply.github.com>
2025-09-28 15:13:58 +0800

    fix: import error

--- scripts/web_driver/driver.py
+++ scripts/web_driver/driver.py
@@ -12,8 +12,6 @@ import time
 import sys
 import os
 
-from scripts.utils import AudioPlayer
-
 if __name__ == "__main__" or os.path.dirname(sys.argv[0])==os.path.dirname(__file__):
     sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
